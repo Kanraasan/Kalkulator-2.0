ffa9718d016cb0dcc8a59cbc004a6091797d8fd1
Kanraasan <fazza.nauvaldy@gmail.com>
2023-04-08 04:40:37 +0700

    fixing some errors here and there

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const display1El = document.querySelectorAll(".display-1");
-const display2El = document.querySelectorAll(".display-2");
-const tempResultEl = document.querySelectorAll(".temp-result");
+const display1El = document.querySelector(".display-1");
+const display2El = document.querySelector(".display-2");
+const tempResultEl = document.querySelector(".temp-result");
 const numbersEl = document.querySelectorAll(".number");
 const operationEl = document.querySelectorAll(".operation");
-const equalEl = document.querySelectorAll(".equal");
-const clearAllEl = document.querySelectorAll(".all-clear");
-const clearLastEl = document.querySelectorAll(".last-entity-clear");
+const equalEl = document.querySelector(".equal");
+const clearAllEl = document.querySelector(".all-clear");
+const clearLastEl = document.querySelector(".last-entity-clear");
 
 let dis1Num = "";
 let dis2Num = "";
